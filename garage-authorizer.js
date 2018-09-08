ee38eb7831f05a98f2c6d6a065206a8092e21506
Warren Volz <warren@warrenvolz.com>
2018-09-07 23:34:43 -0600

    Config file, door open and state check
    - basic functionality to open the door based on inbound data
      from the reader
    - config file (and template) to hold info to connect to the
      particle cloud API (for garage control)
    - additional modules for parsing CSVs and handling HTTP requests
    - function to check door state, only request open if it's closed

--- garage-authorizer.js
+++ garage-authorizer.js
@@ -1,21 +1,22 @@
 var net = require('net');
 var http = require('http');
+var https = require('https');
+var got  = require('got');
 var csv = require('csv');
+var config = require('./config.js');
 
 var server = net.createServer(function(socket) {
     console.log('client connected from ', socket.remoteAddress);
     socket.setEncoding('utf8');
+    var data = '';
     socket.on('end', function() {
         console.log('client disconnected');
-    });
-    // end of message = \0 \u0000 or NUL
-    socket.on('data', function(data) {
-        //console.log(data);
-        in_data = data.split("\0");
+        // end of message = \0 \u0000 or NUL
+        var in_data = data.split("\0");
         in_data.forEach(function(x) {
             // TODO move to a function
             // broken out by newlines
-            x_lines = x.split("\r\n");
+            var x_lines = x.split("\r\n");
             x_lines.forEach(function(line) {
                 const hashComment = /(#.*)/;
                 if (hashComment.test(line))
@@ -51,6 +52,10 @@ var server = net.createServer(function(socket) {
                 });
             });
         });
+    });
+    socket.on('data', function(chunk) {
+        //console.log(data);
+        data += chunk;
     });
     socket.on('error', function(e) {
         // TODO how do we handle other errors?
@@ -94,7 +99,7 @@ function post_tagscan(data) {
 
 function authorize_tag(tag) {
     // note in line below a= is the authorization ie: garage = 1
-    // http://localhost:3000/tags/E200001715120070171065FE/authorize.json?a=1
+    // http://localhost:3000/tags/1234566ef/authorize.json?a=1
     http.get('http://localhost:3000/tags/'+tag+'/authorize.json?a=1', function(res) {
         const { statusCode } = res;
         const contentType = res.headers['content-type'];
@@ -130,6 +135,7 @@ function authorize_tag(tag) {
                 if (parsedData['response'] == 'authorized')
                 {
                     //call garage open?
+                    get_door_state(processDoorState);
                     console.log('Successful authorization');
                 }
             }
@@ -143,6 +149,102 @@ function authorize_tag(tag) {
     });
 
     return;
+};
+
+function get_door_state(callback) {
+    // check door state, return 1 if down, 0 if up
+    // TODO below should be configurable 
+    let api_url = config.particle.api_url;
+    let device_id = config.particle.device_id;
+    let access_token = config.particle.access_token;
+
+    let url = api_url+device_id+"/doorstate?access_token="+access_token;
+
+    let error = '';
+    console.log('get door state ', url);
+    // get state from particle API
+    let result = 'up'; //default to up?
+    https.get(url, function(res) {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+        //console.log('headers:', res.headers);
+        //console.log('statusCode:', res.statusCode);
+
+        let error;
+        if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+                `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+            error = error.message;
+            console.error(error);
+            // consume response data to free up memory
+            res.resume();
+            return;
+        }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            console.log(parsedData);
+            result = parsedData['result']; //error handling on this?
+            //console.log(result);
+          } catch (e) {
+            console.error(e.message);
+          }
+          callback && callback(error, result);
+        });
+    }).on('error', function(e) {
+        console.error(`door state api call error: ${e.message}`);
+    });
+};
+
+function processDoorState (error, state)  {
+    if (error) return console.error("ERROR", error)
+    if (state == 'down')
+    {
+        console.log("Door down, opening door");
+        openDoor(error);
+    } else {
+        console.log("Door up, no action ", state);
+    }
+};
+
+function openDoor (error) {
+    if (error) return console.error("ERROR", error)
+    // TODO below should be configurable 
+    let api_url = config.particle.api_url;
+    let device_id = config.particle.device_id;
+    let access_token = config.particle.access_token;
+
+    let url = api_url+device_id+"/door1move?access_token="+access_token;
+
+    console.log("APIcall=",url);
+
+    const options = {
+        method: 'POST'
+    };
+
+    got(
+        url, options
+    ).then( (response) => {
+        let parsedData = '';
+        if(response.body)
+        {
+            parsedData = JSON.parse(response.body);
+            console.log(parsedData);
+        }
+        console.log(response.statusCode);
+    }).catch( (error) => {
+        console.log("Got error = "+error.statusMessage);
+        console.log("error = "+error);
+    });
 };
 
 server.on('error', function(err) {
